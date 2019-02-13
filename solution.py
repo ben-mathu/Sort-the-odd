55c808bf4c36228e19338cc90ea7425bf93f4b35
Benard Mathu <mathubenard@gmail.com>
2019-02-13 22:22:35 +0300

    Comments: pseudo-codes

--- solution.py
+++ solution.py
@@ -8,3 +8,10 @@ def sort_array(source_array):
                     source_array[i] = source_array[j]
                     source_array[j] = aux
     return source_array
+// for each element in source_array count i
+//     for each element in source _array starting from i
+//         if remainder of element at i / 2 == 1 && remainder of element at j / 2 == 1
+//             check if element at j < element at i
+//                 aux = element at i
+//                 element at i = element at j
+//                 element at j = aux
